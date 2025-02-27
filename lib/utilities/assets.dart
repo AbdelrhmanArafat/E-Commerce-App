1a6bb3ebf3b8c1d37460c98bfe7cfcb8038c9d1a
AbdelrhmanArafat <abdelrhmanarafatahmed@gmail.com>
2025-02-27 17:49:17 +0200

    edit App Assets class

--- lib/utilities/assets.dart
+++ lib/utilities/assets.dart
@@ -2,7 +2,6 @@ class AppAssets {
   /// Home Page Images
   static const String topBannerHomePageAsset =
       'https://t3.ftcdn.net/jpg/01/38/94/62/360_F_138946263_EtW7xPuHRJSfyl4rU2WeWmApJFYM0B84.jpg';
-  // 'https://www.thoughtco.com/thmb/ctxxtfGGeK5f_-S3f8J-jbY-Gp8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/close-up-of-clothes-hanging-in-row-739240657-5a78b11f8e1b6e003715c0ec.jpg';
 
   static const String tempProductAsset1 =
       'https://m.media-amazon.com/images/I/61-jBuhtgZL._UX569_.jpg';
